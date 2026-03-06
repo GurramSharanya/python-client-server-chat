f77de18815adb0cb3a9e3ae8a121decda4f782dd
sharanyagurram <sharanyagurram9@gmail.com>
2026-03-06 19:09:14 +0530

    Clean up server.py by removing comments
    Removed comments for cleaner code.

--- server.py
+++ server.py
@@ -1,22 +1,21 @@
 import socket
 
-# create socket
+
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# bind host and port
 server_socket.bind(("localhost", 9999))
 
-# start listening
+
 server_socket.listen(1)
 
 print("Server started... Waiting for connections")
 
-# accept connection
+
 conn, addr = server_socket.accept()
 print("Connected with", addr)
 
 while True:
-    # receive message from client
+    
     data = conn.recv(1024).decode()
 
     if not data:
