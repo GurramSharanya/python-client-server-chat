634d01303ab8aa40baf7e58d3d27ae82d76fa802
sharanyagurram <sharanyagurram9@gmail.com>
2026-03-06 19:10:02 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,17 +1,17 @@
 import socket
 
-# create socket
+
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# connect to server
+
 client_socket.connect(("localhost", 9999))
 
 while True:
-    # send message
+  
     message = input("Client: ")
     client_socket.send(message.encode())
 
-    # receive reply
+    
     data = client_socket.recv(1024).decode()
     print("Server:", data)
 
